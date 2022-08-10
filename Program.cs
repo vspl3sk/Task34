d257d9c88b154ec4d7deff4674eb7182b82d766d
vspl3sk <vspl3sk@yandex.ru>
2022-08-10 12:44:38 +0300

    Задача на подсчет количества четных чисел в массиве из трёхзначных чисел

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] CreateArrayRndInd (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[");
+        if(i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInd(4, 100, 999);
+PrintArray(array);
+
+int count = 0;
+for (int a = 0; a < array.Length; a++)
+{
+    if (array[a] % 2 == 0)
+    count++;
+}
+Console.WriteLine(" ");
+Console.Write($"Чётных чисел в массиве -> {count}");
